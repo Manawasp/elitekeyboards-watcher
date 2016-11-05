6eb5b08f95c453fd1537ed38c8a59b764cd11399
Kyndt Clovis <clovis.kyndt@epitech.eu>
2016-11-05 08:34:28 +0100

    reorder struct keyboard + comment

--- structs.go
+++ structs.go
@@ -1,13 +1,15 @@
 package main
 
-type Keyboards struct {
-	Keyboards map[string]Keyboard
-}
-
+// Keyboard type regroup informations about keyboard on http://elitekeyboards.com
 type Keyboard struct {
 	Name      string
 	Price     string
 	Model     string
 	Image     string
 	Available bool
+}
+
+// Keyboards TODO: doc
+type Keyboards struct {
+	Keyboards map[string]Keyboard
 }
